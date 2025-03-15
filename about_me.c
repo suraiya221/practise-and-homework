3906726cf0f1a136cc1da03536999c86e951cbd2
suraiya221 <suraiyajannat050@gmail.com>
2025-03-16 01:12:56 +0600

    Add files via upload

--- about_me.c
+++ about_me.c
@@ -1,16 +1,39 @@
 #include <stdio.h>
+int main(){
+    printf("[Suraiya] Project member Information \n");
+    // Aurhter dent information
+    printf("===================================== \n");
+    printf("NAME\t\t|Arthur dent\t\t|Male \n");
+    printf("Major\t\t| Mechanical Engineering\tExperience\t| 15 years\n");
+    printf("Role\t\t| Architect\t\tSkills\t\t| C/C++, Java, Spring, Python, Machine Learning/Deep Learning\n");
+    printf("Introduction\t| I built my career working on various projects in SNS, fintech, HR, and media in Silicon Valley. Through successes and failures, I have gained insights I want to share with you.\n");
+    //kim youngjin information
+    printf("------------------------------------ \n");
+    printf("NAME\t\t|kim youngjin\t\t|Male \n");
+    printf("Major\t\t|Computer Engineering\tExperience\t|2 years\n");
+    printf("Role\t\t| Developer\t\tSkills\t\t| C#, Python, JavaScript, React, Kotlin\n");
+    printf("Introduction\t| Recently, I found immense passion for software development through a personal asset management project using the MAUI framework. Solving real-world problems with my code is incredible! Successfully developing Magrathea has now become one of my bucket list goals.\n");
+    //im woncheol information
+    printf("------------------------------------ \n");
+    printf("NAME\t\t|Im Woncheol\t\t|Male \n");
+    printf("Major\t\t|Electronic Engineering \tExperience\t|1 years\n");
+    printf("Role\t\t|DBA\t\tSkills\t\t|Java, PHP, MongoDB, MS SQL, MySQL\n");
+    printf("Introduction\t| I believe that applications are ultimately about how they handle data. While studying Big Data courses in university, I developed a deep interest in data platforms. I think the success of this project depends on data. Just thinking about the data we'll manage in Magrathea already excites me!\n");
+    // Yoo geoun information
+    printf("------------------------------------ \n");
+    printf("NAME\t\t|Yoo Geoun\t\t|Female \n");
+    printf("Major\t\t|Management Information Systems\tExperience\t|1 year\n");
+    printf("Role\t\t|Cloud Engineer\t\tSkills\t\t|Java, PowerShell, Azure, AWS, GCP\n");
+    printf("Introduction\t| I worked on a project with Kim Youngjin, where I was responsible for implementing cloud architecture. During my school years, I became fascinated with cloud computing, particularly Microsoft Azure. As I delved deeper, I found that understanding other public cloud platforms became much easier. I am determined to complete Magrathea's cloud architecture with my own hands!\n");
+    //Seo hyekyung information
+    printf("------------------------------------ \n");
+    printf("NAME\t\t|Seo hyekyung\t\t|Female \n");
+    printf("Major\t\t|Political Science & International Relations\tExperience\t|0 year\n");
+    printf("Role\t\t|Developer \t\tSkills\t\t|Python, Swift, Kotlin, Node.js, Figma\n");
+    printf("Introduction\t| While I have worked on many projects as academic assignments, this is my first real-world project where a company's success is at stake. I am both excited and nervous about whether I can perform well. When I first heard about the Magrathea project, something inside me told me that this was a project worth dedicating myself to. Having already heard of Arthur's reputation through LinkedIn, I feel honored to be part of this journey.\n");
 
-int main()
+    return 0;  
 
-{
-   printf("[kim youngjin]project member Introduction \n");
-   printf("---------------------------------------------");
-   printf("NAME   | kim youngjin \n");
-   printf("GENDER | MALE  \n");
-   printf("MAJOR  | Computer Engineering  \n");
-   printf("EXPERIENCE  | 2years  \n");
-   printf("ROLE   | Developer  \n");
-   printf("SKILLS  | C#, Python, JavaScript, React, Kotlin  \n");
-   printf("INTRODUCTION   |Recently, I found immense passion for software development through a personal asset management project using the MAUI framework. Solving real-world problems with my code is incredible! Successfully developing Magrathea has now become one of my bucket list goals.  \n");
-   return 0;
+
+
 }
