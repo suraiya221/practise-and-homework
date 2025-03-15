fd1e9318a33fd19b88155d865d18693fbd510252
suraiya221 <suraiyajannat050@gmail.com>
2025-03-15 19:15:26 +0600

    Add files via upload

---
+++ about_me.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+
+{
+   printf("[kim youngjin]project member Introduction \n");
+   printf("---------------------------------------------");
+   printf("NAME   | kim youngjin \n");
+   printf("GENDER | MALE  \n");
+   printf("MAJOR  | Computer Engineering  \n");
+   printf("EXPERIENCE  | 2years  \n");
+   printf("ROLE   | Developer  \n");
+   printf("SKILLS  | C#, Python, JavaScript, React, Kotlin  \n");
+   printf("INTRODUCTION   |Recently, I found immense passion for software development through a personal asset management project using the MAUI framework. Solving real-world problems with my code is incredible! Successfully developing Magrathea has now become one of my bucket list goals.  \n");
+   return 0;
+}
