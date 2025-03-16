7bf772f1716327dcaf5a93fb703a20ecda5c62d1
suraiya221 <suraiyajannat050@gmail.com>
2025-03-16 20:56:08 +0600

    Add files via upload

--- about_me.c
+++ about_me.c
@@ -4,20 +4,20 @@ int main(){
     // Aurhter dent information
     printf("================================================================================== \n");
     printf("NAME\t\t        |Arthur dent\t\t                       Gender\t\t|Male \n");
-    printf("Major\t\t       | Mechanical Engineering \t\t     Experience\t\t |15 years\n");
+    printf("Major\t\t       | Mechanical Engineering \t\t     Experience\t\t |00000000 00000000 00000000 00001111\n");
     printf("Role\t\t        | Architect\t\t                     Skills\t\t   | C/C++, Java, Spring, Python, Machine Learning/Deep Learning\n");
     printf("Introduction\t\t| I built my career working on various projects in SNS, fintech, HR, and media in Silicon Valley.\n");
     printf(" Through successes and failures, I have gained insights I want to share with you.\n");
     //kim youngjin information
     printf("--------------------------------------------------------------------------------------- \n");
     printf("NAME\t\t        |kim youngjin\t\t               Gender\t\t       |Male \n");
-    printf("Major\t\t       |Computer Engineering\t\t   Experience\t\t       |2year \n");
+    printf("Major\t\t       |Computer Engineering\t\t   Experience\t\t       |00000000 00000000 00000000 00000010 \n");
     printf("Role\t\t        | Developer\t\t              Skills\t\t          | C#, Python, JavaScript, React, Kotlin\n");
     printf("Introduction\t\t| Recently, I found immense passion for software development through a personal asset management project using the MAUI framework.Solving real-world problems with my code is incredible! Successfully developing Magrathea has now become one of my bucket list goals.\n");
     //im woncheol information
     printf("------------------------------------ ---------------------------------------------------------\n");
     printf("NAME\t\t        |Im Woncheol\t\t                Gender\t\t        |Male \n");
-    printf("Major\t\t       |Electronic Engineering\t\t  Experience\t\t       |1 years\n");
+    printf("Major\t\t       |Electronic Engineering\t\t  Experience\t\t       |00000000 00000000 00000000 00000001\n");
     printf("Role\t\t        |DBA\t\t                     Skills\t\t           |Java, PHP, MongoDB, MS SQL, MySQL\n");
     printf("Introduction\t\t| I believe that applications are ultimately about how they handle data.\n");
     printf("\t\t While studying Big Data courses in university, I developed a deep interest in data platforms.\n");
@@ -25,12 +25,12 @@ int main(){
     // Yoo geoun information
     printf("------------------------------------------------------------------------------- \n");
     printf("NAME\t\t        |Yoo Geoun\t\t                          Gender\t\t|Female \n");
-    printf("Major\t\t       |Management Information Systems\t\t Experience\t\t|1 year\n");
+    printf("Major\t\t       |Management Information Systems\t\t Experience\t\t|00000000 00000000 00000000 00000001\n");
     printf("Role\t\t        |Cloud Engineer\t\t                 Skills\t\t    |Java, PowerShell, Azure, AWS, GCP\n");
     printf("Introduction\t\t| I worked on a project with Kim Youngjin where I was responsible for implementing cloud architecture. During my school years. I became fascinated with cloud computing, particularly Microsoft Azure. As I delved deeper.I found that understanding other public cloud platforms became much easier. I am determined to co \n");
     printf("------------------------------------------------------------------------------- \n");
     printf("NAME\t\t         |Seo hyekyung\t\t                                    Gender\t\t     |Female \n");
-    printf("Major\t\t        |Political Science & International Relations\t\t     Experience\t\t |0 year\n");
+    printf("Major\t\t        |Political Science & International Relations\t\t     Experience\t\t |00000000 00000000 00000000 00000000\n");
     printf("Role\t\t         |Developer \t\t                                  Skills\t\t         |Python, Swift, Kotlin, Node.js, Figma\n");
     printf("Introduction:\t\t| While I have worked on many projects as academic assignments, this is my first real-world project where a company's success is at stake. I am both excited and nervous about whether I can perform well. When I first heard about the Magrathea project, something inside me told me that this was a project worth dedicating myself to.Having already heard of Arthur's reputation through LinkedIn, I feel honored to be part of this journey.\n");
 
